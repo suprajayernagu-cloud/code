c99c99327fb7eacd789413dd4ae0313b0c199c58
suprajayernagu-cloud <suprajayernagu13@gmail.com>
2026-01-25 19:28:31 +0530

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { JOBS_URL } from '../config'
-import './Home.css'
+import './home.css'
 
 const ITEMS_PER_PAGE = 6
 
